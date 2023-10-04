d7ea09edac187243373a5379cc6d2392e243db6d
progneo <prograneo@gmail.com>
2023-10-04 23:32:52 +0800

    Create test authorization

--- app/src/main/java/com/enplus/energetic/MainActivity.kt
+++ app/src/main/java/com/enplus/energetic/MainActivity.kt
@@ -6,12 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.compose.material3.Surface
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
+import com.enplus.energetic.data.preference.AuthStateManager
 import com.enplus.energetic.ui.component.navigation.NavGraph
 import com.enplus.energetic.ui.theme.EnergeticTheme
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var authStateManager: AuthStateManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +27,10 @@ class MainActivity : ComponentActivity() {
             EnergeticTheme {
                 Surface {
                     val navController = rememberNavController()
-                    NavGraph(navController = navController)
+                    NavGraph(
+                        navController = navController,
+                        isAuthorized = authStateManager.authState,
+                    )
                 }
             }
         }
