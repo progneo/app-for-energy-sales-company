d79f1cd6e14fa009eaeca2a4e27419b663c0a0b1
imysko <il.mysko@gmail.com>
2023-10-05 20:23:48 +0800

    Created drop down item with dynamic content

---
+++ app/src/main/java/com/enplus/energetic/ui/components/dropDownItem/DropDownItem.kt
@@ -0,0 +1,93 @@
+package com.enplus.energetic.ui.components.dropDownItem
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ExpandLess
+import androidx.compose.material.icons.outlined.ExpandMore
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.enplus.energetic.ui.theme.EnergeticTheme
+
+@Composable
+fun DropDownItem(
+    modifier: Modifier = Modifier,
+    title: @Composable () -> Unit,
+    isExpanded: Boolean = false,
+    onExpandRequest: () -> Unit,
+    content: @Composable () -> Unit,
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth(),
+        shape = MaterialTheme.shapes.small,
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(MaterialTheme.shapes.small)
+                .clickable { onExpandRequest() }
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                title()
+
+                Icon(
+                    imageVector = if (isExpanded)
+                        Icons.Outlined.ExpandLess else Icons.Outlined.ExpandMore,
+                    contentDescription = null,
+                )
+            }
+        }
+
+        if (isExpanded) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp),
+            ) {
+                content()
+            }
+        }
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun DropDownItemPreview() {
+    var expanded by remember { mutableStateOf(false) }
+
+    EnergeticTheme {
+        DropDownItem(
+            title = { Text(text = "Something text") },
+            isExpanded = expanded,
+            onExpandRequest = { expanded = !expanded },
+            content = {
+                Text(text = "Something text 1")
+                Text(text = "Something text 2")
+                Text(text = "Something text 3")
+            },
+        )
+    }
+}
