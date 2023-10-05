ee9ae4b94b7f2cf6c580e2b58b4afc757c2df8d6
imysko <il.mysko@gmail.com>
2023-10-05 18:05:03 +0800

    Prevented making screenshots and video recordings

--- app/src/main/java/com/enplus/energetic/MainActivity.kt
+++ app/src/main/java/com/enplus/energetic/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.enplus.energetic
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Surface
@@ -15,6 +16,11 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_SECURE,
+            WindowManager.LayoutParams.FLAG_SECURE
+        )
 
         installSplashScreen()
 
