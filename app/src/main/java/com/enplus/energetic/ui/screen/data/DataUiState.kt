59c639bee2b9abb6c4f99df5d747cb0d91fe5727
imysko <il.mysko@gmail.com>
2023-11-06 19:52:25 +0800

    Added the not found state and improved the filter

--- app/src/main/java/com/enplus/energetic/ui/screen/data/DataUiState.kt
+++ app/src/main/java/com/enplus/energetic/ui/screen/data/DataUiState.kt
@@ -5,4 +5,5 @@ sealed class DataUiState {
     data object Loading : DataUiState()
     data object Error : DataUiState()
     data object Content : DataUiState()
+    data object ContentWithoutMeters : DataUiState()
 }
--- app/src/main/java/com/enplus/energetic/ui/util/Mocks.kt
+++ app/src/main/java/com/enplus/energetic/ui/util/Mocks.kt
@@ -89,6 +89,113 @@ object Mocks {
                     ),
                 ),
             ),
+            PersonDataResponse(
+                personAccountId = 123457,
+                address = "Ломоносова 13",
+                metersList = listOf(
+                    MeterApi(
+                        category = CategoryTypeApi.ELECTRICAL_ENERGY,
+                        type = "FBU 11205",
+                        state = true,
+                        factoryNumber = 112280081,
+                        placing = "КРУ",
+                        checkDate = LocalDate.of(2023, 9, 19),
+                        lastCheckDate = LocalDate.of(2023, 9, 21),
+                        sealState = true,
+                        lastReadings = listOf(
+                            ReadingApi(
+                                date = LocalDate.of(2023, 9, 20),
+                                value = 123123,
+                            ),
+                            ReadingApi(
+                                date = LocalDate.of(2023, 10, 20),
+                                value = 123123,
+                            ),
+                            ReadingApi(
+                                date = LocalDate.of(2023, 11, 20),
+                                value = 123123,
+                            ),
+                        ),
+                    ),
+                    MeterApi(
+                        category = CategoryTypeApi.HOT_WATER_SUPPLY,
+                        type = "АГАТ 1-2",
+                        state = true,
+                        factoryNumber = 112280081,
+                        placing = "КРУ",
+                        checkDate = LocalDate.of(2023, 9, 19),
+                        lastCheckDate = LocalDate.of(2023, 9, 21),
+                        sealState = true,
+                        lastReadings = listOf(
+                            ReadingApi(
+                                date = LocalDate.of(2023, 9, 20),
+                                value = 123123,
+                            ),
+                            ReadingApi(
+                                date = LocalDate.of(2023, 10, 20),
+                                value = 123123,
+                            ),
+                            ReadingApi(
+                                date = LocalDate.of(2023, 11, 20),
+                                value = 123123,
+                            ),
+                        ),
+                    ),
+                    MeterApi(
+                        category = CategoryTypeApi.COLD_WATER_SUPPLY,
+                        type = "АГАТ 1-1",
+                        state = true,
+                        factoryNumber = 112280081,
+                        placing = "КРУ",
+                        checkDate = LocalDate.of(2023, 9, 19),
+                        lastCheckDate = LocalDate.of(2023, 9, 21),
+                        sealState = true,
+                        lastReadings = listOf(
+                            ReadingApi(
+                                date = LocalDate.of(2023, 9, 20),
+                                value = 123123,
+                            ),
+                            ReadingApi(
+                                date = LocalDate.of(2023, 10, 20),
+                                value = 123123,
+                            ),
+                            ReadingApi(
+                                date = LocalDate.of(2023, 11, 20),
+                                value = 123123,
+                            ),
+                        ),
+                    ),
+                    MeterApi(
+                        category = CategoryTypeApi.THERMAL_ENERGY,
+                        type = "DBB 13201",
+                        state = true,
+                        factoryNumber = 112280081,
+                        placing = "КРУ",
+                        checkDate = LocalDate.of(2023, 9, 19),
+                        lastCheckDate = LocalDate.of(2023, 9, 21),
+                        sealState = true,
+                        lastReadings = listOf(
+                            ReadingApi(
+                                date = LocalDate.of(2023, 9, 20),
+                                value = 123123,
+                            ),
+                            ReadingApi(
+                                date = LocalDate.of(2023, 10, 20),
+                                value = 123123,
+                            ),
+                            ReadingApi(
+                                date = LocalDate.of(2023, 11, 20),
+                                value = 123123,
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+            PersonDataResponse(
+                personAccountId = 123458,
+                address = "Ломоносова 14",
+                metersList = emptyList(),
+            ),
         )
 
         return personDataList.firstOrNull { personData ->
