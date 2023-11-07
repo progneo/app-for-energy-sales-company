b8da76b6d188f581c273ceb897fb002f3d36de81
v.kolosov <v.kolosov@tinkoff.ru>
2023-11-07 18:46:01 +0800

    add mock data

--- app/src/main/java/com/enplus/energetic/ui/util/Mocks.kt
+++ app/src/main/java/com/enplus/energetic/ui/util/Mocks.kt
@@ -92,7 +92,7 @@ object Mocks {
                 ),
             ),
             PersonDataResponse(
-                personAccountId = 12345790,
+                personAccountId = 123456790,
                 address = "ул. Ломоносова д. 12 кв. 35",
                 metersList = listOf(
                     MeterApi(
@@ -194,43 +194,262 @@ object Mocks {
                 ),
             ),
             PersonDataResponse(
-                personAccountId = 12345791,
+                personAccountId = 123456791,
                 address = "ул. Ломоносова д. 12 кв. 36",
                 metersList = emptyList(),
             ),
             PersonDataResponse(
-                personAccountId = 12345792,
-                address = "ул. Ломоносова д. 12 кв. 36",
+                personAccountId = 123456792,
+                address = "ул. Ломоносова д. 12 кв. 37",
+                metersList = listOf(
+                    MeterApi(
+                        category = CategoryTypeApi.THERMAL_ENERGY,
+                        type = "FBU 11205",
+                        state = true,
+                        factoryNumber = 112280081,
+                        placing = "КРУ",
+                        checkDate = LocalDate.of(2023, 9, 19),
+                        lastCheckDate = LocalDate.of(2023, 9, 21),
+                        sealState = false,
+                        lastReadings = listOf(
+                            ReadingApi(
+                                date = LocalDate.of(2023, 9, 20),
+                                value = 123123,
+                            ),
+                            ReadingApi(
+                                date = LocalDate.of(2023, 10, 20),
+                                value = 123123,
+                            ),
+                            ReadingApi(
+                                date = LocalDate.of(2023, 11, 20),
+                                value = 123123,
+                            ),
+                        ),
+                    ),
+                    MeterApi(
+                        category = CategoryTypeApi.HOT_WATER_SUPPLY,
+                        type = "АГАТ 1-2",
+                        state = true,
+                        factoryNumber = 112280081,
+                        placing = "КРУ",
+                        checkDate = LocalDate.of(2023, 9, 19),
+                        lastCheckDate = LocalDate.of(2023, 9, 21),
+                        sealState = true,
+                        lastReadings = listOf(
+                            ReadingApi(
+                                date = LocalDate.of(2023, 9, 20),
+                                value = 123123,
+                            ),
+                            ReadingApi(
+                                date = LocalDate.of(2023, 10, 20),
+                                value = 123123,
+                            ),
+                            ReadingApi(
+                                date = LocalDate.of(2023, 11, 20),
+                                value = 123123,
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+            PersonDataResponse(
+                personAccountId = 123456793,
+                address = "ул. Ломоносова д. 12 кв. 38",
+                metersList = listOf(
+                    MeterApi(
+                        category = CategoryTypeApi.ELECTRICAL_ENERGY,
+                        type = "FBU 11205",
+                        state = true,
+                        factoryNumber = 112280081,
+                        placing = "КРУ",
+                        checkDate = LocalDate.of(2023, 9, 19),
+                        lastCheckDate = LocalDate.of(2023, 9, 21),
+                        sealState = false,
+                        lastReadings = listOf(
+                            ReadingApi(
+                                date = LocalDate.of(2023, 9, 20),
+                                value = 123123,
+                            ),
+                            ReadingApi(
+                                date = LocalDate.of(2023, 10, 20),
+                                value = 123123,
+                            ),
+                            ReadingApi(
+                                date = LocalDate.of(2023, 11, 20),
+                                value = 123123,
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+            PersonDataResponse(
+                personAccountId = 123456794,
+                address = "ул. Ломоносова д. 12 кв. 39",
+                metersList = listOf(
+                    MeterApi(
+                        category = CategoryTypeApi.COLD_WATER_SUPPLY,
+                        type = "FBU 11205",
+                        state = false,
+                        factoryNumber = 112280081,
+                        placing = "КРУ",
+                        checkDate = LocalDate.of(2023, 9, 19),
+                        lastCheckDate = LocalDate.of(2023, 9, 21),
+                        sealState = false,
+                        lastReadings = listOf(
+                            ReadingApi(
+                                date = LocalDate.of(2023, 9, 20),
+                                value = 123123,
+                            ),
+                            ReadingApi(
+                                date = LocalDate.of(2023, 10, 20),
+                                value = 123123,
+                            ),
+                            ReadingApi(
+                                date = LocalDate.of(2023, 11, 20),
+                                value = 123123,
+                            ),
+                        ),
+                    ),
+                    MeterApi(
+                        category = CategoryTypeApi.COLD_WATER_SUPPLY,
+                        type = "FBU 11205",
+                        state = true,
+                        factoryNumber = 112280081,
+                        placing = "КРУ",
+                        checkDate = LocalDate.of(2023, 9, 19),
+                        lastCheckDate = LocalDate.of(2023, 9, 21),
+                        sealState = false,
+                        lastReadings = listOf(
+                            ReadingApi(
+                                date = LocalDate.of(2023, 9, 20),
+                                value = 123123,
+                            ),
+                            ReadingApi(
+                                date = LocalDate.of(2023, 10, 20),
+                                value = 123123,
+                            ),
+                            ReadingApi(
+                                date = LocalDate.of(2023, 11, 20),
+                                value = 123123,
+                            ),
+                        ),
+                    ),
+                    MeterApi(
+                        category = CategoryTypeApi.HOT_WATER_SUPPLY,
+                        type = "FBU 11205",
+                        state = true,
+                        factoryNumber = 112280081,
+                        placing = "КРУ",
+                        checkDate = LocalDate.of(2023, 9, 19),
+                        lastCheckDate = LocalDate.of(2023, 9, 21),
+                        sealState = false,
+                        lastReadings = listOf(
+                            ReadingApi(
+                                date = LocalDate.of(2023, 9, 20),
+                                value = 123123,
+                            ),
+                            ReadingApi(
+                                date = LocalDate.of(2023, 10, 20),
+                                value = 123123,
+                            ),
+                            ReadingApi(
+                                date = LocalDate.of(2023, 11, 20),
+                                value = 123123,
+                            ),
+                        ),
+                    ),
+                    MeterApi(
+                        category = CategoryTypeApi.HOT_WATER_SUPPLY,
+                        type = "FBU 11205",
+                        state = true,
+                        factoryNumber = 112280081,
+                        placing = "КРУ",
+                        checkDate = LocalDate.of(2023, 9, 19),
+                        lastCheckDate = LocalDate.of(2023, 9, 21),
+                        sealState = false,
+                        lastReadings = listOf(
+                            ReadingApi(
+                                date = LocalDate.of(2023, 9, 20),
+                                value = 123123,
+                            ),
+                            ReadingApi(
+                                date = LocalDate.of(2023, 10, 20),
+                                value = 123123,
+                            ),
+                            ReadingApi(
+                                date = LocalDate.of(2023, 11, 20),
+                                value = 123123,
+                            ),
+                        ),
+                    ),
+                    MeterApi(
+                        category = CategoryTypeApi.ELECTRICAL_ENERGY,
+                        type = "FBU 11205",
+                        state = true,
+                        factoryNumber = 112280081,
+                        placing = "КРУ",
+                        checkDate = LocalDate.of(2023, 9, 19),
+                        lastCheckDate = LocalDate.of(2023, 9, 21),
+                        sealState = false,
+                        lastReadings = listOf(
+                            ReadingApi(
+                                date = LocalDate.of(2023, 9, 20),
+                                value = 123123,
+                            ),
+                            ReadingApi(
+                                date = LocalDate.of(2023, 10, 20),
+                                value = 123123,
+                            ),
+                            ReadingApi(
+                                date = LocalDate.of(2023, 11, 20),
+                                value = 123123,
+                            ),
+                        ),
+                    ),
+                    MeterApi(
+                        category = CategoryTypeApi.THERMAL_ENERGY,
+                        type = "FBU 11205",
+                        state = true,
+                        factoryNumber = 112280081,
+                        placing = "КРУ",
+                        checkDate = LocalDate.of(2023, 9, 19),
+                        lastCheckDate = LocalDate.of(2023, 9, 21),
+                        sealState = false,
+                        lastReadings = listOf(
+                            ReadingApi(
+                                date = LocalDate.of(2023, 9, 20),
+                                value = 123123,
+                            ),
+                            ReadingApi(
+                                date = LocalDate.of(2023, 10, 20),
+                                value = 123123,
+                            ),
+                            ReadingApi(
+                                date = LocalDate.of(2023, 11, 20),
+                                value = 123123,
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+            PersonDataResponse(
+                personAccountId = 123456795,
+                address = "ул. Ломоносова д. 12 кв. 40",
                 metersList = emptyList(),
             ),
             PersonDataResponse(
-                personAccountId = 12345793,
-                address = "ул. Ломоносова д. 12 кв. 36",
+                personAccountId = 123456796,
+                address = "ул. Ломоносова д. 12 кв. 41",
                 metersList = emptyList(),
             ),
             PersonDataResponse(
-                personAccountId = 12345794,
-                address = "ул. Ломоносова д. 12 кв. 36",
+                personAccountId = 123456797,
+                address = "ул. Ломоносова д. 12 кв. 42",
                 metersList = emptyList(),
             ),
             PersonDataResponse(
-                personAccountId = 12345795,
-                address = "ул. Ломоносова д. 12 кв. 36",
-                metersList = emptyList(),
-            ),
-            PersonDataResponse(
-                personAccountId = 12345796,
-                address = "ул. Ломоносова д. 12 кв. 36",
-                metersList = emptyList(),
-            ),
-            PersonDataResponse(
-                personAccountId = 12345797,
-                address = "ул. Ломоносова д. 12 кв. 36",
-                metersList = emptyList(),
-            ),
-            PersonDataResponse(
-                personAccountId = 12345798,
-                address = "ул. Ломоносова д. 12 кв. 36",
+                personAccountId = 123456798,
+                address = "ул. Ломоносова д. 12 кв. 43",
                 metersList = emptyList(),
             ),
         )
@@ -256,7 +475,14 @@ object Mocks {
                 FlatDataApi(
                     personId = (123456789 + i).toLong(),
                     flatNumber = 34 + i,
-                    metersCount = (0..6).random(),
+                    metersCount = when (i) {
+                        0 -> 3
+                        1 -> 4
+                        3 -> 2
+                        4 -> 1
+                        5 -> 6
+                        else -> 0
+                    },
                 )
             )
         }
